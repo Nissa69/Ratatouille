f28aaf3d2b9de12b94b743cae1174474f18c335e
CilooW <cecile.angays@yahoo.fr>
2016-09-13 16:49:54 +0200

    changé l'inclusion du header et footer en php, page d'accueil nommée index.php pour correspondre aux bonnes pratiques

---
+++ Projetratatouille/header.php
@@ -0,0 +1,10 @@
+<header style="color:white">
+ <nav class="navbar navbar-inverse navbar-fixed-top">
+            <ul class="nav navbar-nav">
+                <li><a>Accueil</a></li>
+                <li><a href="quisommesnous.html">qui sommes-nous</a></li>
+                <li><a href="restaurants.html">nos restaurants</a></li>
+                <li><a href="franchise.html">Devenir franchisé</a></li>
+            </ul>
+        </nav> 
+ </header>
---
+++ Projetratatouille/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+        <title>Restaurant Ratatouille</title>
+        <meta charset="utf-8">
+        <meta name="Restaurant Ratatouille cuisine traditionnelle Liscence ratatouille" content="165c. uniques">
+        
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+        <link href="fds.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <div id="page" class="container-fluid" >
+    
+<?php include ("header.php") ?>
+        
+          
+
+            
+
+            
+
+    
+          
+  
+    <div class="">
+    <h1 style="margin-top:100px">Restaurant Ratatouille</h1>
+     
+    </div>
+
+    
+<? php include ("footer.php") ?>
+</div>
+       
+        
+    
+</body>
+</html>
