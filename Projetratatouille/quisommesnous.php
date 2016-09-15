3be7a2323eb6c91454b08545dbc5b0b0bc9c1e8c
Nissa69 <yann.chanteloup.pro@gmail.com>
2016-09-15 11:45:23 +0200

    finalisation mise en forme qsn

--- Projetratatouille/quisommesnous.php
+++ Projetratatouille/quisommesnous.php
@@ -29,16 +29,18 @@
 					En offrant une gamme de produits respectueuse de notre environnement, Ratatouille s’inspire au mieux de la cuisine du marché afin de respecter les saisons. 
     				</p>	
 			</div>
-			<div id ="haut_de_page" class="col-xs-4">
-				<img src="img/bio.JPG" class="img-responsive">
+				<div id="haut_de_page" class="col-xs-4">
+					<img src="img/bio.JPG" class="img-responsive">
+					</div>
 			</div>
-		</div>
 
 		<div class="row">
-			<div class="col-xs-4">
+
+			<div id="milieu_de_page" class="col-xs-4">
 				<img src="img/100.jpg" class="img-responsive">
 			</div>
-			<div class="col-xs-8">
+			
+			<div id="param" class="col-xs-8">
 				<h2>
 					Un équilibre alimentaire !
 				</h2>
@@ -46,13 +48,41 @@
 					Chaque repas, vous prendrez 3 des 5 portions recommandées sans même y penser!
 					Les menus sont équilibrés, apportant le midi la juste proportion d’aliments dont nous avons besoin pour être en forme : 3 portions de fruits et légumes, 2 portions de céréales ou féculents, 1 portion de viandes, poisson ou oeuf, 1 portion de laitage*.
 				</p>
+				<p>
+					* Il est recommandé chaque jour de consommer :
+				</p>
+				<ul>
+					<li> 5 fruits et légumes</li>
+					<li> 2 viandes</li>
+					<li> 2 poissons</li>
+					<li> 2 oeufs</li>
+					<li> 6 céréales ou féculents</li>
+					<li> 3 laitages</li>
+				</ul>
 			</div>
-			
 		</div>
+		<div class="row">
 
-
-			
-    
+			<div id="parab" class="col-xs-8">
+				<h2>
+					Des produits de qualité !
+				</h2>
+				<p>
+					Des champs à l'assiette...
+				</p>
+				<p>
+					La plupart de nos légumes et fruits de saison viennent en direct de chez des producteurs locaux (Picardie) qui privilégient l’agriculture biologique (utilisation curative des pesticides, respect de l’environnement et des saisons !) eh oui, eh oui....
+				</p>
+				<p>
+					L’économie sociale version Ratatouille, c’est le commerce de proximité…
+					En plus, nos céréales sont issues de l'agriculture biologique !!! 
+				</p>
+			</div>
+			<div id="bas_de_page" class="col-xs-4">
+				<img src="img/carot.jpg" class="img-responsive">
+			</div>
+		</div>	
+		
 	</section>
        
         
