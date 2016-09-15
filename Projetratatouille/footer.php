04dd94afd7c8444ebde86f1faca8e60e287e2573
Nissa69 <yann.chanteloup.pro@gmail.com>
2016-09-15 09:35:30 +0200

    ajout contenu qsn et correction indentation générale

--- Projetratatouille/footer.php
+++ Projetratatouille/footer.php
@@ -1,7 +1,7 @@
- <footer class="col-xs-12">
+<footer class="col-xs-12">
         <div class="row text-center">
             <p class="col-xs-4 footerdiv"> Mentions légales </p>
             <p class="col-xs-4">contact</p>
             <p class="col-xs-4">devenir franchisé</p>
-            </div>
-        </footer>
+        </div>
+</footer>
--- Projetratatouille/quisommesnous.php
+++ Projetratatouille/quisommesnous.php
@@ -12,18 +12,48 @@
 </head>
 
 <body class="quisommesnous">
-    <div id="page" class="container" >
-    
 <?php include ("header.php") ?>
-          
-<div>
-    
-</div>
-   
 
+    <section id="Aboutpage" class="container" >
+     
+		<div class="row">
+			<div class="col-xs-8">
+				<h1>
+					Qui sommes nous ?
+				</h1>
+				<div class="soustitre">
+					<p>Un savoir faire Français</p>
+				</div>
+					<p>Créée en 2015, la société a pour objectif de proposer aux amoureux de la gastronomie française un nouveau concept axé sur une restauration alliant fraîcheur des produits et saveurs gustatives.</p>
+					<p>
+					En offrant une gamme de produits respectueuse de notre environnement, Ratatouille s’inspire au mieux de la cuisine du marché afin de respecter les saisons. 
+    				</p>	
+			</div>
+			<div id ="haut_de_page" class="col-xs-4">
+				<img src="https://lh3.googleusercontent.com/-dPww4nfQ-rI/VfmaW47VueI/AAAAAAAACF4/GP8CWTVlHio/w426-h320/Bio%2Bgreen%252C%2BMix%2Bvegetables.JPG" class="img-responsive">
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-4">
+				<img src="http://scontent.cdninstagram.com/t51.2885-15/s480x480/e15/11111302_578347362268124_1793974677_n.jpg?ig_cache_key=OTYzMzE2NTk0MzUyODE5MzMw.2" class="img-responsive">
+			</div>
+			<div class="col-xs-8">
+				<h2>
+					Un équilibre alimentaire !
+				</h2>
+				<p>
+					Chaque repas, vous prendrez 3 des 5 portions recommandées sans même y penser!
+					Les menus sont équilibrés, apportant le midi la juste proportion d’aliments dont nous avons besoin pour être en forme : 3 portions de fruits et légumes, 2 portions de céréales ou féculents, 1 portion de viandes, poisson ou oeuf, 1 portion de laitage*.
+				</p>
+			</div>
+			
+		</div>
+
+
+			
     
- 
-</div>
+	</section>
        
         
     
